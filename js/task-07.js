e82e438422a0099e43ce1c14455a4b26bbd3f15e
CharlieLive <karol.kozaczuk@gmail.com>
2023-01-30 19:17:22 +0100

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -2,7 +2,7 @@ const input = document.querySelector(`#font-size-control`);
 const text = document.querySelector(`#text`);
 
 input.addEventListener(`input`, controller);
-
+input.value = input.min;
 function controller() {
-    text.style.fontSize = input.value + "px";
+  text.style.fontSize = input.value + "px";
 }
